f4e241554926c7fc68b51ce1cf23b0b4ff47e257
elvin <tnma@birentech.com>
2023-08-23 14:31:40 +0800

    add vim_env

---
+++ mtn_vimrc/vim_env.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+cd && mkdir -p .vim && cd .vim
+
+# install vim 8.2
+git clone git@github.com:vim/vim.git
+cd vim
+git checkout v8.2.0108
+#./configure --prefix=$HOME/.local --enable-python3interp=yes
+./configure --enable-python3interp=yes
+make -j16
+sudo make install
+
+
+# ctags + taglist + cscope prepare
+sudo apt-get install -y exuberant-ctags
+sudo apt-get install cscope
+wget https://sourceforge.net/projects/vim-taglist/files/vim-taglist/4.6/taglist_46.zip/download
+unzip download
+ctags --fields=+iaS --extra=+q -R -f ~/.vim/tags /usr/include /usr/local/include
+
+echo "alias cs_get='find `pwd` -name *.cpp -o -name *.h -o -name *.hpp> cscope.files && cscope -Rbq'" >> ~/.bashrc
+echo "alias ct_get='ctags -R .'" >> ~/.bashrc
+echo "alias cc_get='ctags -R && find `pwd` -name *.cpp -o -name *.h -o -name *.hpp cscope.files && cscope -Rbq'" >> ~/.bashrc
+
+# install ripgrep
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+sudo dpkg -i ripgrep_13.0.0_amd64.deb
+
